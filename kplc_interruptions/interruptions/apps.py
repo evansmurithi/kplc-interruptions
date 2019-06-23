88de60adeb19e56d18ed3cf5c17673a03f1dd581
Evans Murithi <murithievans80@gmail.com>
2019-06-23 15:00:04 +0300

    Create migration files

--- kplc_interruptions/interruptions/apps.py
+++ kplc_interruptions/interruptions/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class InterruptionsConfig(AppConfig):
-    name = 'interruptions'
+    name = 'kplc_interruptions.interruptions'
--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interruptions.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE', 'kplc_interruptions.config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
